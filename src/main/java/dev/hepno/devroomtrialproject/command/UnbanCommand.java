d55373156fb092281fae3ae589ab54354e0a521d
hepno <me@hepno.dev>
2023-04-01 14:55:27 +0200

    add(UnbanCommand): add full unban command

--- src/main/java/dev/hepno/devroomtrialproject/command/UnbanCommand.java
+++ src/main/java/dev/hepno/devroomtrialproject/command/UnbanCommand.java
@@ -1,7 +1,11 @@
 package dev.hepno.devroomtrialproject.command;
 
 import dev.hepno.devroomtrialproject.manager.Command;
+import dev.hepno.devroomtrialproject.manager.DatabaseManager;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 
@@ -18,6 +22,34 @@ public class UnbanCommand extends Command {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
+        DatabaseManager databaseManager = new DatabaseManager();
+
+        // Connect to database
+        try {
+            databaseManager.connect();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        // Ensure command is used correctly
+        if (args.length == 0) {
+            sender.sendMessage("You must specify a player to unban!");
+            return;
+        }
+
+        if (args.length > 1) {
+            sender.sendMessage("Too many arguments!");
+            return;
+        }
+
+        OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+
+        if (databaseManager.isBanned(target.getUniqueId())) {
+            databaseManager.deleteBan(target.getUniqueId());
+            sender.sendMessage("Player " + args[0] + " has been unbanned!");
+        } else {
+            sender.sendMessage("Player " + args[0] + " is not banned. Did you mean to use /ban?");
+        }
 
     }
 
