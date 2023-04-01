02791c7c5896cbe287569db0db590feece77f371
hepno <me@hepno.dev>
2023-04-01 14:37:20 +0200

    style(BanListener): improve the way the kick message shows the remaining time for temporary bans

--- src/main/java/dev/hepno/devroomtrialproject/event/BanListener.java
+++ src/main/java/dev/hepno/devroomtrialproject/event/BanListener.java
@@ -11,6 +11,12 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.Date;
 import java.util.UUID;
 
 public class BanListener implements Listener {
@@ -29,9 +35,56 @@ public class BanListener implements Listener {
         if (databaseManager.isBanned(uuid)) {
             Timestamp bannedAt = databaseManager.getBannedAt(uuid);
             Timestamp expiresAt = databaseManager.getBanExpiresAt(uuid);
+            Timestamp now = new Timestamp(System.currentTimeMillis());
+
+            // Get the units needed to create the message
+            Duration difference = Duration.between(now.toLocalDateTime(), expiresAt.toLocalDateTime());
+            long seconds = difference.getSeconds();
+            long minutes = seconds / 60;
+            long hours = minutes / 60;
+            long days = hours / 24;
+            long weeks = days / 7;
+            long months = days / 30;
+
+            // Remove units used (eg, if there are 2 days, remove 48 hours)
+            seconds -= minutes * 60;
+            minutes -= hours * 60;
+            hours -= days * 24;
+            days -= weeks * 7;
+            weeks -= months * 4;
+
+            // Create a string to display the time remaining (a bit messy, but it works)
+            String message = "In ";
+            if (months > 0) {
+                message += months + " months, ";
+            }
+
+            if (weeks > 0) {
+                message += weeks + " weeks, ";
+            }
+
+            if (days > 0) {
+                message += days + " days, ";
+            }
+
+            if (hours > 0) {
+                message += hours + " hours, ";
+            }
+
+            if (minutes > 0) {
+                message += minutes + " minutes, ";
+            }
+
+            if (seconds > 0) {
+                message += seconds + " seconds, ";
+            }
+
 
             System.out.println(bannedAt);
             System.out.println(expiresAt);
+            System.out.println(expiresAt);
+            System.out.println(difference);
+            System.out.println(message);
 
             if (expiresAt == null) {
                 event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_BANNED);
@@ -39,9 +92,8 @@ public class BanListener implements Listener {
             } else if (expiresAt.after(new Timestamp(System.currentTimeMillis()))) {
                 event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_BANNED);
                 event.setKickMessage(ChatColor.RED + "You have been banned from this server for " +
-                        ChatColor.WHITE + "\"" + databaseManager.getReason(uuid) + "\"" + ChatColor.RED +
-                        " until " + ChatColor.WHITE + expiresAt + ChatColor.RED + "!")
-                ;
+                databaseManager.getReason(uuid) + ". You will be unbanned " + message
+                );
             } else {
                 // The server does not check if the ban has expired periodically, so we need to remove it from the database if it has
                 databaseManager.deleteBan(uuid);
